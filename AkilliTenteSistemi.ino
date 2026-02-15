5c7fa2841fa281d4e9c3a834612e1918d4a6def0
Aslan akbey <agmroc@gmail.com>
2026-02-15 21:16:45 +0300

    Update AkilliTenteSistemi.ino

--- AkilliTenteSistemi.ino
+++ AkilliTenteSistemi.ino
@@ -8,23 +8,42 @@ int led_blue = 4;
 //Sensörler
 int rainlevel_sensor = A0;
 
+//Butonlar
+int btn_mod1 = 7;
+int btn_mod2 = 8;
+
 //Durum değişkenleri
 int terim = 255;
 int sayac = 0;
 int kontrol_edildi = 0;
 
+//Motorlar
+Servo x_servo;
+Servo y_servo;
+
 void setup(){
   Serial.begin(9600);
+  
+  //Ledlerin pin modları
   pinMode(led_red,OUTPUT);
   pinMode(led_blue,OUTPUT);
-  Serial.println('Fonksiyon başlatılıyor');
+
+  //Butonların pin modları
+  pinMode(btn_mod1,INPUT_PULLUP);
+  pinMode(btn_mod2,INPUT_PULLUP);
+
+  //Motorları ata
+  x_servo.attach(13);
+  y_servo.attach(12);
+
+  //Test fonksiyonu başlat;
+  Serial.println("Fonksiyon başlatılıyor");
   ledTestFunction();
 }
 
 void loop(){
  int analog_value = analogRead(rainlevel_sensor);
  int mapped_value = map(analog_value,0,250,0,1);
- Serial.println(mapped_value);
 
   if(mapped_value < 1){
     digitalWrite(led_blue,LOW);
@@ -36,6 +55,30 @@ void loop(){
     digitalWrite(led_blue,HIGH);
   }
 
+  int btn_mod1_digital_state = digitalRead(btn_mod1);
+  int btn_mod2_digital_state = digitalRead(btn_mod2);
+
+  Serial.print("Durum-1: ");
+  Serial.print(btn_mod1_digital_state);
+  Serial.print("Durum-2: ");
+  Serial.print(btn_mod2_digital_state);
+  Serial.print("\n");
+
+  if(btn_mod1_digital_state + btn_mod2_digital_state != 2){
+    for(int nx = 0; nx < 2; nx ++){
+      if(nx == 1){
+        x_servo.write(0);
+        y_servo.write(0);
+        break;
+      }
+      x_servo.write(90);
+      y_servo.write(90);
+      delay(100);
+      x_servo.write(0);
+      y_servo.write(0);
+      delay(100);
+    }
+  }
 }
 
 //Led Test fonksiyonu(ILK BAŞTA ÇALIŞACAK OLAN)
