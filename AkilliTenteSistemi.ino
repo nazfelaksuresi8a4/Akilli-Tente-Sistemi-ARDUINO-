d45be09f53e61e4951fd79b11a381c154213b164
Aslan akbey <agmroc@gmail.com>
2026-02-15 22:31:21 +0300

    Update AkilliTenteSistemi.ino

--- AkilliTenteSistemi.ino
+++ AkilliTenteSistemi.ino
@@ -23,7 +23,7 @@ Servo y_servo;
 
 void setup(){
   Serial.begin(9600);
-  
+
   //Ledlerin pin modları
   pinMode(led_red,OUTPUT);
   pinMode(led_blue,OUTPUT);
@@ -33,8 +33,14 @@ void setup(){
   pinMode(btn_mod2,INPUT_PULLUP);
 
   //Motorları ata
-  x_servo.attach(13);
-  y_servo.attach(12);
+  x_servo.attach(9);
+  y_servo.attach(10);
+
+  x_servo.write(0);
+  y_servo.write(0);
+
+  x_servo.write(16);
+  y_servo.write(16);
 
   //Test fonksiyonu başlat;
   Serial.println("Fonksiyon başlatılıyor");
@@ -42,14 +48,13 @@ void setup(){
 }
 
 void loop(){
- int analog_value = analogRead(rainlevel_sensor);
- int mapped_value = map(analog_value,0,250,0,1);
+  int analog_value = analogRead(rainlevel_sensor);
+  int mapped_value = map(analog_value,0,250,0,1);
 
   if(mapped_value < 1){
     digitalWrite(led_blue,LOW);
     digitalWrite(led_red,HIGH);
   }
-
   else if(mapped_value > 0){
     digitalWrite(led_red,LOW);
     digitalWrite(led_blue,HIGH);
@@ -64,22 +69,18 @@ void loop(){
   Serial.print(btn_mod2_digital_state);
   Serial.print("\n");
 
-  if(btn_mod1_digital_state + btn_mod2_digital_state != 2){
+  if(btn_mod1_digital_state != 1 && btn_mod2_digital_state != 1){
+    calibrate_servo();
     for(int nx = 0; nx < 2; nx ++){
-      if(nx == 1){
-        x_servo.write(0);
-        y_servo.write(0);
-        break;
-      }
-      x_servo.write(90);
-      y_servo.write(90);
-      delay(100);
-      x_servo.write(0);
-      y_servo.write(0);
-      delay(100);
+      x_servo.write(60);
+      y_servo.write(60);
+      delay(500);
+      x_servo.write(-60);
+      y_servo.write(-60);
     }
   }
 }
+
 
 //Led Test fonksiyonu(ILK BAŞTA ÇALIŞACAK OLAN)
 void ledTestFunction(){
@@ -95,7 +96,6 @@ void ledTestFunction(){
       digitalWrite(led_blue,LOW);
       delay(100);
       Serial.println(sayac);
-      
       sayac ++;
     }
     else{
@@ -106,4 +106,12 @@ void ledTestFunction(){
       loop();
     }
   }
+}
+
+void calibrate_servo(){
+      int y_deg = y_servo.read();
+      int x_deg = x_servo.read();
+
+      y_servo.write(y_deg);
+      x_servo.write(x_deg);
 }
