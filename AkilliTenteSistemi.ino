e26ecf79679faa9d9eecbc642c23ebfb1845173e
Aslan akbey <agmroc@gmail.com>
2026-02-22 08:25:50 +0300

    Update AkilliTenteSistemi.ino

--- AkilliTenteSistemi.ino
+++ AkilliTenteSistemi.ino
@@ -1,139 +1,157 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 #include <Servo.h>
-#include <LiquidCrystal.h>
 
-//ledler
-int led_red = 2;
-int led_blue = 4;
+//LCD SCREEN
+LiquidCrystal_I2C lcd(0x27,16,2);
 
-//Sensörler
-int rainlevel_sensor = A0;
+//Buzzer
+int buzzer = 5;
 
-//Butonlar
-int btn_mod1 = 7;
-int btn_mod2 = 8;
+//lamps
+int lamp_1 = 7;
+int lamp_2 = 8;
+int lamp_3 = 9;
 
-//Motorlar
-Servo x_servo;
-Servo y_servo;
+//Servos
+Servo servo_x;
+Servo servo_y;
 
-//Buzzerlar 
-int buzzer_1 = 5;
+//Sensors
+int rainlevel_sensor = A0; 
 
-//Durum değişkenleri
-int terim = 255;
-int sayac = 0;
-int kontrol_edildi = 0;
+//Static&Dynamic vals
+int maxval = 55;
+bool closed = false;
 
-int y_servo_deg = -180;
-int x_servo_deg = -180;
-
-
+int absoulte_deg_x = 0;
+int absoulte_deg_y = 0;
 
 void setup(){
   Serial.begin(9600);
-
-  //Ledlerin pin modları
-  pinMode(led_red,OUTPUT);
-  pinMode(led_blue,OUTPUT);
-
-  //Butonların pin modları
-  pinMode(btn_mod1,INPUT_PULLUP);
-  pinMode(btn_mod2,INPUT_PULLUP);
-
-  //Buzzer pin modları
-  pinMode(buzzer_1,OUTPUT);
-
-  //Motorları ata
-  x_servo.attach(9);
-  y_servo.attach(10);
-
-  x_servo.write(0);
-  y_servo.write(0);
-
-  x_servo.write(16);
-  y_servo.write(16);
-
-  //Test fonksiyonu başlat;
-  digitalWrite(buzzer_1,HIGH);
-  delay(1000);
-  digitalWrite(buzzer_1,LOW);
   
-  Serial.println("Fonksiyon başlatılıyor");
-  ledTestFunction();
-  calibrate_servo();
+  lcd.begin();
+  lcd.print("TEST-----TEST");
+
+  //PinModes
+  pinMode(buzzer,OUTPUT);
+  pinMode(lamp_1,OUTPUT);
+  pinMode(lamp_2,OUTPUT);
+  pinMode(lamp_3,OUTPUT);
+  pinMode(rainlevel_sensor,INPUT);
+
+  //Servo attachs
+  servo_x.attach(3);
+  servo_x.attach(10);
+  servo_x.write(0);
+  servo_y.write(0);
+
+  //Function calls
+  testFunction();
+
 }
 
 void loop(){
-  int analog_value = analogRead(rainlevel_sensor);
-  int mapped_value = map(analog_value,0,250,0,1);
+  int analog_rainlevel_sensor_value = analogRead(rainlevel_sensor);
+  int mapped_analog_sensor_val = map(analog_rainlevel_sensor_value,1023,0,100,0);
+  Serial.println(mapped_analog_sensor_val); 
 
-  if(mapped_value < 1){
-    digitalWrite(led_blue,LOW);
-    digitalWrite(led_red,HIGH);
-    calibrate_servo();
-  }
-  else if(mapped_value > 0){
-    digitalWrite(led_red,LOW);
-    digitalWrite(led_blue,HIGH);
-    x_servo.write(+90);
-    y_servo.write(+90);
-  }
-
-  int btn_mod1_digital_state = digitalRead(btn_mod1);
-  int btn_mod2_digital_state = digitalRead(btn_mod2);
-
-  Serial.print("Durum-1: ");
-  Serial.print(btn_mod1_digital_state);
-  Serial.print("Durum-2: ");
-  Serial.print(btn_mod2_digital_state);
-  Serial.print("\n");
-
-  if(btn_mod1_digital_state != 1 && btn_mod2_digital_state != 1){
-    for(int nx = 0; nx < 90; nx ++){
-      x_servo.write(x_servo_deg);
-      y_servo.write(y_servo_deg);
-
-      x_servo_deg ++;
-      y_servo_deg --;
-
-      delay(40);
-
-
+  if(mapped_analog_sensor_val >= maxval){
+    if(closed == false){
+      closed = true;
+      if(servo_x.read() == absoulte_deg_x && servo_y.read() == absoulte_deg_y)
+      for(int n = 0; n < 10; n++){
+        delay(100);
+        lcd.clear();
+        lcd.print("!TENTE KAPANIYOR!");
+        digitalWrite(buzzer,1);
+        digitalWrite(lamp_1,1);
+        delay(100);
+        lcd.clear();
+        digitalWrite(buzzer,0);
+        digitalWrite(lamp_1,0);
+      }
+      for(int degre = 0; degre < 90; degre ++){
+        servo_x.write(degre);
+        servo_y.write(-degre);
+        delay(50);
+      }
     }
-    calibrate_servo();
   }
-}
+  else{
+    if(closed == true){
+      
+      for(int tx = 20; tx > 0; tx--){
+        if(mapped_analog_sensor_val >= maxval){
+          closed = "0xf";
+          break;
+        } 
+        lcd.setCursor(0,0);
+        lcd.print(tx);
+        lcd.print(" Saniye sonra");
+        lcd.setCursor(0,1);
+        lcd.print("Tente acilacak"); 
+        delay(1000);
+      }
 
+      if(closed == "0xf"){
+        while(1){
 
-//Led Test fonksiyonu(ILK BAŞTA ÇALIŞACAK OLAN)
-void ledTestFunction(){
-  while(terim >= 255){
-    if(sayac < 4){
-      Serial.println(terim);
-      digitalWrite(led_red,HIGH);
-      delay(100);
-      digitalWrite(led_red,LOW);
-      delay(100);
-      digitalWrite(led_blue,HIGH);
-      delay(100);
-      digitalWrite(led_blue,LOW);
-      delay(100);
-      Serial.println(sayac);
-      sayac ++;
+        }
+      }
+      else{
+        servo_x.write(absoulte_deg_x);
+        servo_y.write(absoulte_deg_y);
+        lcd.clear();
+        closed = false;
+      }
+
     }
     else{
-      terim = -255;
-      sayac = 0;
-      Serial.println(terim,sayac);
-      break;
-      loop();
+      lcd.clear();
+      lcd.print("islaklik: ");
+      lcd.print(" %");
+      lcd.print(mapped_analog_sensor_val);
+      delay(50);
     }
+    
   }
 }
 
-void calibrate_servo(){
-      y_servo_deg = 180;
-      x_servo_deg = 0;
-      y_servo.write(y_servo_deg);
-      x_servo.write(x_servo_deg);
+void testFunction(){
+  //Buzzer test
+  digitalWrite(buzzer,1);
+  delay(500);
+  digitalWrite(buzzer,0);
+
+  //Lamp & Servo test
+  digitalWrite(lamp_1,1);
+  delay(150);
+  digitalWrite(lamp_2,1);
+  servo_x.write(90);
+  servo_y.write(-90);
+  delay(4000);
+  digitalWrite(lamp_3,1);
+  delay(150);
+  digitalWrite(lamp_1,0);
+  delay(150);
+  digitalWrite(lamp_2,0);
+  servo_x.write(0);
+  servo_y.write(0);
+  delay(4000);
+  digitalWrite(lamp_3,0);
+  delay(150);
+  digitalWrite(lamp_2,1);
+
+  //Reset lcd
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Akilli Tente");
+  delay(500);
+  lcd.setCursor(0,1);
+  lcd.print("Sistemi 1.0");
+  delay(1200);
+
+  absoulte_deg_x = servo_x.read();
+  absoulte_deg_y = servo_y.read();
 }
